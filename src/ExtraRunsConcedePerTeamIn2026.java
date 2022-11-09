7c5ec5c4ded922bba3955929bdb554fbdd8cb4b8
sachin <bhandarisachin7567@gmail.com>
2022-11-09 21:15:39 +0530

    created new file to get extra runs conceded by each team in 2016

--- src/ExtraRunsConcedePerTeamIn2026.java
+++ src/ExtraRunsConcedePerTeamIn2026.java
@@ -1,2 +1,19 @@
-public class ExtraRunsConcedePerTeamIn2026 {
+import java.util.Set;
+
+class ExtraRunsConcedePerTeamIn2016  {
+    ExtraRunsConcedePerTeamIn2016(){
+        System.out.println("TEAM                           EXTRA RUNS");
+    }
+    String get(){
+        ConvertingRawDataToDictionaryForm obj= new ConvertingRawDataToDictionaryForm(filePath);
+        Set<String> setOfKeys =obj.storeDeliveries.keySet();
+        for (String key : obj.storeDeliveries.keySet()){
+
+        }
+
+        return "treu";
+    }
+
 }
+
+
